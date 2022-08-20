6625e8bdec2fa8d9c67d38613c4000a62755bd93
Mayur Badhe <badhemayur01@gmail.com>
2022-08-20 16:27:12 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+puublic class Multiply{
+
+    public static vid main (String []args){
+        int a=10;
+        int b=20;
+        System.out.println("multiplication is a* b ="+a*b);
+
+    }
+
+}
