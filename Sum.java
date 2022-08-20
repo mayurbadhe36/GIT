435787293b5b48af4e273b1c9653964952650585
Mayur Badhe <badhemayur01@gmail.com>
2022-08-20 16:16:36 +0530

    added msg in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class Sum{
     public void static main (String []args)
     int a=5;
     int b=6;
-    System.out.println(a+b);
+    System.out.println("The sum of two number is "+a+b);
 
 }
